f9512106e3660e42a9331ac8c259ddd802f72038
Romit Padghane <romitpadghane15@gmail.com>
2024-05-24 00:21:42 +0530

    added new file

--- Main.java
+++ Main.java
@@ -8,6 +8,7 @@ public static void main(String[] args) {
     System.out.println("output from java program");
     System.err.println(new Date());
     System.err.println("changes");
+    System.err.println("added new file");
     
 }
 }
