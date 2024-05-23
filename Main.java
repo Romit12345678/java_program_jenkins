e7c7d0c5ebf3a4d85cb7fd95b2d04afcb522cadb
Romit Padghane <romitpadghane15@gmail.com>
2024-05-23 23:57:48 +0530

    initial commit

--- Main.java
+++ Main.java
@@ -7,6 +7,7 @@ public static void main(String[] args) {
     System.out.println("this is java program");
     System.out.println("output from java program");
     System.err.println(new Date());
+    System.err.println("changes");
     
 }
 }
