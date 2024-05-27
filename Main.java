9a98d405a574697e11fc420ae9fe6027600010e3
Romit Padghane <romitpadghane15@gmail.com>
2024-05-27 14:58:31 +0530

    changes in main

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public static void main(String[] args) {
     System.err.println("hello ");
     System.err.println("hello from java program ");
     System.err.println("h r u?? ");
+    System.err.println("i am good ");
 
 }
 
